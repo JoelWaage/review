7804346f656412cae4dc7d1dc3e4f281a13900d7
Joel <joeltwaage@gmail.com>
2016-09-09 13:07:48 -0700

    add show-new form and ability to save the information into the database

--- app/components/show-new.js
+++ app/components/show-new.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewShow: false,
+  actions: {
+    showFormDisplay() {
+      this.set('addNewShow', true);
+    },
+
+    save1() {
+      var params = {
+        title: this.get("title"),
+        location: this.get("location"),
+        genre: this.get("genre"),
+        image: this.get("image"),
+        description: this.get("description")
+      };
+      this.set('addNewShow', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('show');
   },
+
+  actions: {
+    save3(params) {
+      var newShow = this.store.createRecord('show', params);
+      newShow.save();
+      this.transitionTo('index');
+    },
+  }
 });
